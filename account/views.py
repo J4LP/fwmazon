60ee8165ef49161bdbdf45f79269464bbf51666c
adrien-f <adrien.fillon@gmail.com>
2013-06-24 12:12:53 +0000

    Added price to element model, and custom queryset methods

--- account/views.py
+++ account/views.py
@@ -1,9 +1,19 @@
-from django.views.generic import TemplateView
+from fwmazon.views import FwmazonTemplateView
 
 
-class AccountHomeView(TemplateView):
+class AccountHomeView(FwmazonTemplateView):
     template_name = "account/account.html"
 
-    def get_context_data(self, **kwargs):
+    def get_request_context_data(self, request, **kwargs):
         context = super(AccountHomeView, self).get_context_data(**kwargs)
+        context['orders'] = request.user.orders.all()
+        return context
+
+
+class AccountOrdersView(FwmazonTemplateView):
+    template_name = "account/orders.html"
+
+    def get_request_context_data(self, request, **kwargs):
+        context = super(AccountOrdersView, self).get_context_data(**kwargs)
+        context['orders'] = request.user.orders.all()
         return context
