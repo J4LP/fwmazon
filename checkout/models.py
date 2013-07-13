d3f4ed8f65363dfe2323f8f5d172775fcf576936
adrien-f <adrien.fillon@gmail.com>
2013-07-13 21:19:21 +0000

    Modified price calculations, volume display

--- checkout/models.py
+++ checkout/models.py
@@ -54,10 +54,10 @@ class Order(models.Model):
     buyer = models.ForeignKey(User, related_name='orders')
     contractor = models.ForeignKey(User, related_name='orders_contracted', null=True)
     payment = models.ForeignKey('Payment', related_name='order')
-    total_price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=0.00)
-    elements_price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=0.00)
-    volume = models.FloatField()
-    shipping_fee = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=0.00)
+    total_price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=d(0.00))
+    elements_price = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=d(0.00))
+    volume = models.FloatField(default=0.0)
+    shipping_fee = models.DecimalField(max_digits=15, decimal_places=2, blank=True, default=d(0.00))
     shipping_destination = models.ForeignKey(ShippingDestination)
     to_be_fitted = models.BooleanField(default=False)
     priority_flag = models.BooleanField(default=False)
@@ -78,6 +78,11 @@ class Order(models.Model):
         if fitting == 'fitting':
             self.to_be_fitted = True
         self.buyer = user
+        # Calculating prices
+        self.total_price = d(0)
+        self.elements_price = d(0)
+        self.shipping_fee = d(cart.volume) * self.shipping_destination.shipping_cost
+        self.volume = cart.volume
         elements = []
         for doctrine in cart.doctrines:
             elements.append(OrderElement(element_type='doctrine', doctrine_fit=doctrine, amount=doctrine.amount))
@@ -87,10 +92,12 @@ class Order(models.Model):
         for module in cart.modules:
             elements.append(OrderElement(element_type='module', item=module))
         """
-        self.save()
-        p = Payment(order=self)
-        p.key = p.generate_key()
+        self.total_price = self.elements_price + self.shipping_fee + (FITTING_PRICE if self.to_be_fitted else d(0.0)) + self.tax
+        p = Payment()
+        p.generate_key()
         p.save()
+        self.payment = p
+        self.save()
         for e in elements:
             e.order = self
             e.save()
@@ -143,11 +150,11 @@ class OrderElement(models.Model):
 
 class Payment(models.Model):
     status = models.IntegerField(default=WAITING, choices=ORDER_STATUS_CHOICES)
-    key = models.CharField(max_length=64)
+    key = models.CharField(max_length=35)
     transaction_id = models.IntegerField(max_length=15, null=True)
     character_id = models.IntegerField(max_length=15, null=True)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     def generate_key(self):
-        self.key = ''.join(random.choice(string.lowercase + string.uppercase + string.digits) for x in range(64))
+        self.key = ''.join(random.choice(string.lowercase + string.uppercase + string.digits) for x in range(35))
