15a86c4656b65de3928145a90b5360dd99a069e0
adrien-f <adrien.fillon@gmail.com>
2013-07-13 21:17:51 +0000

    Added payment button to account orders

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from fwmazon.views import FwmazonTemplateView
+from django.shortcuts import render_to_response, redirect
+from django.template import RequestContext
 from django.views.generic.base import View
-from django.shortcuts import redirect
 from checkout.models import Order
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
@@ -24,21 +25,19 @@ class AccountOrdersView(FwmazonTemplateView):
         return context
 
 
-class AccountOrderDetailView(FwmazonTemplateView):
+class AccountOrderDetailView(View):
     template_name = "account/order_details.html"
 
-    def get_request_context_data(self, request, **kwargs):
-        context = super(AccountOrderDetailView, self).get_context_data(**kwargs)
+    def get(self, request, order_id):
         try:
-            order = Order.objects.get(pk=kwargs['order_id'])
+            order = Order.objects.get(pk=order_id)
         except Order.DoesNotExist:
             messages.error(request, 'Could not find the order.')
             return redirect('/')
         if order.buyer != request.user:
             messages.error(request, 'Security error, are you logged in ?')
             return redirect('/')
-        context['order'] = order
-        return context
+        return render_to_response(self.template_name, {'order': order}, context_instance=RequestContext(request))
 
 
 class AccountOrderCancelView(View):
