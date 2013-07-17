d358e3bdc2da90ced82cfb4e9d18f2d9cf651fba
adrien-f <adrien.fillon@gmail.com>
2013-07-17 16:52:33 +0000

    Journal processing, updating wallets balance

--- manager/tasks.py
+++ manager/tasks.py
@@ -1,10 +1,16 @@
 from celery import task
-from eve.models import ItemPrice
 from shop.models import DoctrineFit
 from django.utils import timezone
 import json
 from decimal import Decimal
 from eve.models import ItemPrice, InvType
+from evejournal import EveJournal
+import eveapi
+from eve.models import APIKey, CorpWallet, CorpWalletJournalEntry
+from fwmazon.redisevecache import RedisEveAPICacheHandler
+from decimal import Decimal as d
+import datetime
+
 
 # TODO: Add ammo support, and we can do better
 @task()
@@ -31,7 +37,6 @@ def update_fit(fit_id):
         except ItemPrice.DoesNotExist:
             p = ItemPrice()
             p.item = InvType.objects.get(pk=m['id'])
-            p.update_price(force=True)
         p.update_price(force=True)
         price += p.price
     for d in fit['drones']:
@@ -40,7 +45,6 @@ def update_fit(fit_id):
         except ItemPrice.DoesNotExist:
             p = ItemPrice()
             p.item = InvType.objects.get(pk=m['id'])
-            p.update_price(force=True)
         p.update_price(force=True)
         price += (p.price * d['amount'])
     doctrine_fit.price = price
@@ -53,3 +57,53 @@ def update_price_item(item_id):
     item = ItemPrice.objects.get(pk=item_id)
     item.update_price(force=True)
     return item.price
+
+
+@task()
+def process_journal():
+    wallets = CorpWallet.objects.all()
+    for wallet in wallets:
+        journal = EveJournal(wallet.account_key)
+        for page in journal:
+            if page is None:
+                break
+            for transaction in page:
+                try:
+                    entry = CorpWalletJournalEntry.objects.get(ref_id=transaction.refID)
+                except CorpWalletJournalEntry.DoesNotExist:
+                    date = datetime.datetime.utcfromtimestamp(transaction.date)
+                    entry_data = {
+                        'wallet': wallet,
+                        'transaction_date': date,
+                        'ref_type_id': transaction.refTypeID,
+                        'ref_id': transaction.refID,
+                        'sender_name': transaction.ownerName1,
+                        'amount': d(transaction.amount),
+                        'reason': transaction.reason,
+                    }
+                    entry = CorpWalletJournalEntry(**entry_data)
+                    entry.save()
+                    if entry.ref_type_id == 10:
+                        process_transaction.delay(entry.id)
+
+                        #TODO TIMEZONE ACTIVATE TZINFO WAHTEVER
+
+
+@task()
+def process_transaction(transaction_id):
+    raise NotImplementedError
+
+
+@task()
+def update_wallets():
+    api_key = APIKey.objects.get(pk=2338850)
+    api = eveapi.EVEAPIConnection(cacheHandler=RedisEveAPICacheHandler(debug=True)).auth(keyID=api_key.id, vCode=api_key.vcode)
+    balance = api.corp.AccountBalance()
+    for account in balance.accounts:
+        try:
+            wallet = CorpWallet.objects.get(wallet_id=account.accountID)
+        except CorpWallet.DoesNotExist:
+            wallet_data = {'wallet_id': account.accountID, 'account_key': account.accountKey, 'apikey': api_key, 'name': 'Not named #%s' % account.accountID}
+            wallet = CorpWallet(**wallet_data)
+        wallet.balance = d(account.balance)
+        wallet.save()
