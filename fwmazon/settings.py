596df6ff9fc1f79d5c97f5a9ce1c73dcec44f786
adrien-f <adrien.fillon@gmail.com>
2013-07-25 21:53:42 +0000

    Modify wsgi file

--- fwmazon/settings.py
+++ fwmazon/settings.py
@@ -83,7 +83,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'static_files'
+STATIC_ROOT = os.path.join(PROJECT_PATH, '../', 'static_files')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
