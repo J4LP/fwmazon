4267dfa6441bd0edd7e997622f2cf5531aa42952
adrien-f <adrien.fillon@gmail.com>
2013-08-07 22:12:52 +0200

    Add template beta switches and new guideline view

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
-
+from home.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^guidelines$', GuidelinesView.as_view(), name='guidelines'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'home/login.html'}, name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
 )
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render_to_response, redirect
-from django.template import RequestContext
+from django.views.generic import TemplateView
 
 
-def home(req):
-    return render_to_response('home/index.html', context_instance=RequestContext(req))
+class HomeView(TemplateView):
+    template_name = 'home/index.html'
 
+
+class GuidelinesView(TemplateView):
+    template_name = 'home/guidelines.html'
