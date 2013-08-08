dd25c3151903ffe2b8a81dcbac07c1b4e3cfdbd2
adrien-f <adrien.fillon@gmail.com>
2013-08-08 20:20:45 +0200

    Add changepassword view and guidelines updated

--- account/urls.py
+++ account/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns(
         AccountOrderDetailView.as_view()), name='account-order-detail'),
     url(r'^orders/cancel/(?P<order_id>\d+)$', login_required(
         AccountOrderCancelView.as_view()), name='account-order-cancel'),
+    url(r'^password$', 'django.contrib.auth.views.password_change', {'template_name': 'account/password.html', 'post_change_redirect': '/'}, name='account-password'),
 )
